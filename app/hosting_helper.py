3ff0f3a5c3b03dd9aa3da2b460e773be316c737b
rgfriedman <34253667+rgfriedman@users.noreply.github.com>
2021-09-07 15:06:03 -0400

    test

--- app/hosting_helper.py
+++ app/hosting_helper.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import ExtraTreesClassifier
 import os
+import nltk
 os.system('python -m nltk.downloader all')
 from nltk.corpus import stopwords
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
